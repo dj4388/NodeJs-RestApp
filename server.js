ffcb5ac4e3057a275debfd9b7a70107efec1a1de
dj4388 <dj4388@gmail.com>
2017-12-09 12:28:11 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+mongoose.connect('mongodb://localhost/rest_test');
+
+app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use('/api', require('./routes/api'));
+
+// app.get('/',function(req,res){
+// 	res.write('hii there!');
+// 	res.end();
+// });
+
+app.listen(3000);
+
+console.log('server listening at port 3000');
